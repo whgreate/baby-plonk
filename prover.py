e234adef50703c815a4cb2338efc3e34c3d816d4
王会 <>
2024-06-30 17:59:35 +0800

    finish homework_2

--- prover.py
+++ prover.py
@@ -241,6 +241,9 @@ class Prover:
         # reference: https://github.com/sec-bit/learning-zkp/blob/master/plonk-intro-cn/4-plonk-constraints.md
         gate_constraints_coeff = (
             # TODO: your code
+            A_coeff * QL_coeff + B_coeff * QR_coeff +
+            A_coeff * B_coeff * QM_coeff +
+            C_coeff * QO_coeff + PI_coeff + QC_coeff
         )
 
         normal_roots = Polynomial(
@@ -269,6 +272,12 @@ class Prover:
         # reference: https://github.com/sec-bit/learning-zkp/blob/master/plonk-intro-cn/3-plonk-permutation.md
         permutation_grand_product_coeff = (
             # TODO: your code
+            (self.rlc(A_coeff, roots_coeff) * 
+                self.rlc(B_coeff, roots_coeff * Scalar(2)) * 
+                self.rlc(C_coeff, roots_coeff * Scalar(3))) * Z_coeff - 
+            (self.rlc(A_coeff, S1_coeff) * 
+                self.rlc(B_coeff, S2_coeff) * 
+                self.rlc(C_coeff, S3_coeff)) * ZW_coeff
         )
 
         permutation_first_row_coeff = (Z_coeff - Scalar(1)) * L0_coeff
